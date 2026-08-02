35e6526ed772dadd72af6783f2dc59299bbbab04
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2026-08-02 15:32:47 +0200

    Add: longest-common-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-0.go
@@ -0,0 +1,17 @@
+func longestCommonSubsequence(text1 string, text2 string) int {
+    dp := make([][]int, len(text1)+1)
+    for i := range dp {
+        dp[i] = make([]int, len(text2)+1)
+    }
+
+    for i := 1; i < len(dp); i++ {
+        for j := 1; j < len(dp[i]); j++ {
+            if text1[i-1] == text2[j-1] {
+                dp[i][j] = 1+dp[i-1][j-1]
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            }
+        }
+    }
+    return dp[len(dp)-1][len(dp[0])-1]
+}
