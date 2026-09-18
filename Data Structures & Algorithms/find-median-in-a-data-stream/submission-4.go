e6b28386fd8aeed6436607da2bd357dacad6e6af
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2026-09-18 23:22:37 +0200

    Add: find-median-in-a-data-stream - submission-4

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-4.go
@@ -0,0 +1,79 @@
+type MedianFinder struct {
+    minHeap *MinHeap
+	maxHeap *MaxHeap
+}
+
+
+func Constructor() MedianFinder {
+    minheap := &MinHeap{}
+	maxheap := &MaxHeap{}
+	heap.Init(minheap)
+	heap.Init(maxheap)
+	return MedianFinder{minheap, maxheap}
+}
+
+
+func (this *MedianFinder) AddNum(num int)  {
+	heap.Push(this.minHeap, num)
+	heap.Push(this.maxHeap, heap.Pop(this.minHeap))
+	if this.maxHeap.Len() > this.minHeap.Len() {
+		heap.Push(this.minHeap, heap.Pop(this.maxHeap))
+	}
+}
+
+
+func (this *MedianFinder) FindMedian() float64 {
+    if this.minHeap.Len() > this.maxHeap.Len() {
+        return float64((*this.minHeap)[0])
+    }
+
+    return (float64((*this.maxHeap)[0]) + float64((*this.minHeap)[0])) / 2
+}
+
+type MinHeap []int
+
+func (h MinHeap) Len() int {
+	return len(h)
+}
+
+func (h MinHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h MinHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *MinHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MinHeap) Pop() any {
+	x := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return x
+}
+
+type MaxHeap []int
+
+func (h MaxHeap) Len() int {
+	return len(h)
+}
+
+func (h MaxHeap) Less(i, j int) bool {
+	return h[i] > h[j]
+}
+
+func (h MaxHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *MaxHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Pop() any {
+	x := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return x
+}
