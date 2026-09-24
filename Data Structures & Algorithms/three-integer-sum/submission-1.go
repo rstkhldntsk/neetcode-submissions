110310311174011033689574832a1d3b960dd36b
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2026-09-24 14:13:22 +0200

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.go
@@ -0,0 +1,26 @@
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	result := make([][]int, 0)
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		l, r := i+1, len(nums)-1
+		for l < r {
+			curSum := nums[i] + nums[l] + nums[r]
+			if curSum > 0 {
+				r--
+			} else if curSum < 0 {
+				l++
+			} else {
+				result = append(result, []int{nums[i], nums[l], nums[r]})
+				l++
+				r--
+				for l < r && nums[l] == nums[l-1] {
+					l++
+				}
+			}
+		}
+	}
+	return result
+}
