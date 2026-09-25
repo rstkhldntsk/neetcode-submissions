32cc13caed97d19d524a88968d3ddf2f9fa2d6c3
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2026-09-25 15:09:51 +0200

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.go
@@ -0,0 +1,14 @@
+func searchInsert(nums []int, target int) int {
+	l, r := -1, len(nums)
+	for r-l>1 {
+		m := l + (r-l)/2
+		if nums[m] > target {
+			r = m
+		} else if nums[m] < target {
+			l = m
+		} else {
+			return m
+		}
+	}
+	return r
+}
