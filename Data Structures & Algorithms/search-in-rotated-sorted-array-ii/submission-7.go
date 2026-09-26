e899ef8e2de7a56a713806dfbae44465bbbed531
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2026-09-26 16:10:01 +0200

    Add: search-in-rotated-sorted-array-ii - submission-7

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-7.go
@@ -0,0 +1,25 @@
+func search(nums []int, target int) bool {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		m := l + (r-l)/2
+		if nums[m] == target {
+			return true
+		}
+		if nums[l] < nums[m] { // left sorted
+			if nums[l] <= target && target < nums[m] { // target in left
+				r = m - 1
+			} else {
+				l = m + 1
+			}
+		} else if nums[l] > nums[m] { // right sorted
+			if nums[m] < target && target <= nums[r] { // target in right
+				l = m + 1
+			} else {
+				r = m -1
+			}
+		} else { // nums[l] == nums[m]
+			l++
+		}
+	}
+	return false
+}
